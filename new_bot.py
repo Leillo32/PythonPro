a10b088137e61566b7e55c28f775f8d832935235
Leillo32 <flayyclaudia@gmail.com>
2025-11-06 19:09:31 +0100

    Update new_bot.py
    nuevo comando

--- new_bot.py
+++ new_bot.py
@@ -27,3 +27,14 @@ async def joined(ctx, member: discord.Member):
         await ctx.send(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
 
 
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
