13dddffa90219dc3422375c6a0c37f751f698939
Leillo32 <flayyclaudia@gmail.com>
2025-11-27 19:42:31 +0100

    Create hola.py

---
+++ hola.py
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>paginawep</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Hola buenas te ago un resumen del tema de la programacion </h1>
+    <p>Si quieres saverlo no te vallas</p>
+    <h2>Puedes elegir dos opciones de ia</h2>
+             <ul>
+                   <li>chat gpt (la primera)</li>
+                   <li>lleminite la imbentada</li>
+             </ul>
+     <img src="/IMAGENES/descargar.jpg" alt="Image 1">
+</body>
+</html>
+body {
+    font-family: Arial,  sans-serif; /* Familia de fuentes */
+    font-size: 20px; /* El tamaño de la fuente principal en el tag <p></p> */
+    background-color: #ca7411; /* El color del fondo de la página web */
+    color: #f30f0f; /* El color del texto*/ 
+}
+h1 {
+    color: #23dc0e; /* El color de la cabecera */
+    font-size: 24px; /* El tamaño de la fuente de la cabecera */
+    font-family: Georgia, serif; /* La familia tipográfica utilizada en la cabecera */
+}
+img{
+width: 600px;
+margin-left: 700px;
+}
+ul{
+margin-left: 1000px;
+}
